eab84fbccea892d833e990552e420621ec22580f
Kurt Beheydt <kurt@postduif.be>
2024-05-11 16:42:35 +0200

    analog write sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #define pinSensor A0
+#define pinLed 5
 
 uint32_t sensorValue;
 
@@ -9,10 +10,12 @@ void setup() {
     Serial.println();
 
     pinMode(pinSensor, INPUT);
+    pinMode(pinLed, OUTPUT);
 }
 
 void loop() {
     sensorValue = analogRead(pinSensor);
     Serial.println(sensorValue);
+    analogWrite(pinLed, sensorValue);
     delay(100);
 }
