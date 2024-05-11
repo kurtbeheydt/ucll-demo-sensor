247ef08c1a72eb8ed2fc007af4e8d5cbd6b177c3
Kurt Beheydt <kurt@postduif.be>
2024-05-11 16:47:26 +0200

    digital write sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include <Arduino.h>
 
 #define pinSensor A0
-#define pinLed 5
+#define pinLedAnalog 5
+#define pinLedDigital 4
 
 uint32_t sensorValue;
+const uint32_t sensorThreshold = 10;
 
 void setup() {
     Serial.begin(115200);
     Serial.println();
 
     pinMode(pinSensor, INPUT);
-    pinMode(pinLed, OUTPUT);
+    pinMode(pinLedAnalog, OUTPUT);
+    pinMode(pinLedDigital, OUTPUT);
 }
 
 void loop() {
     sensorValue = analogRead(pinSensor);
     Serial.println(sensorValue);
-    analogWrite(pinLed, sensorValue);
+    analogWrite(pinLedAnalog, sensorValue);
+    bool isThresholdReached = (sensorValue < sensorThreshold);
+    digitalWrite(pinLedDigital, isThresholdReached);
     delay(100);
 }
