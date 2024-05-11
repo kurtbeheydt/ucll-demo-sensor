e216d24960b41edfd719b1864fa30cf7c650097a
Kurt Beheydt <kurt@postduif.be>
2024-05-11 17:27:36 +0200

    button

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
 #include <Arduino.h>
 
-#define pinSensor 5
-#define pinLedDigital 4
-
-uint32_t sensorValue;
-const uint32_t sensorThreshold = 10;
+#define pinButton 5
+#define pinLed 4
 
 void setup() {
     Serial.begin(115200);
     Serial.println();
 
-    pinMode(pinSensor, INPUT_PULLUP);
-    pinMode(pinLedDigital, OUTPUT);
+    pinMode(pinButton, INPUT_PULLUP);
+    pinMode(pinLed, OUTPUT);
 }
 
 void loop() {
-    sensorValue = digitalRead(pinSensor);
-    Serial.println(sensorValue);
-    digitalWrite(pinLedDigital, sensorValue);
+    bool buttonValue = !digitalRead(pinButton);
+    Serial.println(buttonValue);
+    digitalWrite(pinLed, buttonValue);
     delay(100);
 }
